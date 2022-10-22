66036ef46f9737be649250e3a868d74ddb7e2d70
Mldamico <37380030+Mldamico@users.noreply.github.com>
2022-10-22 20:19:52 -0300

    Added routes, car interface and mongo connection

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import "dotenv/config"
 import express from 'express'
 import cors from 'cors'
-
+import { router } from "./routes"
+import db from './config/mongo'
 const PORT = process.env.PORT || 8001
 
 const app = express()
 app.use(cors())
-
+app.use(router)
+db().then(() => {
+  console.log("DB connected")
+})
 app.listen(PORT, () => {
   console.log(`Port ${PORT}`)
 })
